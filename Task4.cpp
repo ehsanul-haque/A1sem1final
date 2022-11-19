152e8d43bfb5e52aa7a23d199391077b04a00dd5
Ehsanul Haque <114832270+ehsanul-haque@users.noreply.github.com>
2022-11-19 17:01:36 +0600

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -11,8 +11,16 @@ void Swap(int &a, int &b)
 int main()
 
 {
-    int a = 5, b = 10;
+
+    int a, b;
+    cout << "Enter a ";
+    cin >> a;
+    cout << "Enter b ";
+    cin >> b;
     Swap(a, b);
+
+    cout << "after swap a = " << a << " "
+         << "after swap b = " << b;
 
     return 0;
 }
