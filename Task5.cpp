d9899d0a02bef97206e198f321cc5aa291512b6e
Ehsanul Haque <114832270+ehsanul-haque@users.noreply.github.com>
2022-11-19 17:01:53 +0600

    Update Task5.cpp

--- Task5.cpp
+++ Task5.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 void primeInRange(int R)
 {
-    int L=1;
+    int L = 1;
     int flag;
 
-    for (int i = L; i <= R; i++) {
+    for (int i = L; i <= R; i++)
+    {
 
         if (i == 1 || i == 0)
             continue;
 
         flag = 1;
 
-        for (int j = 2; j <= i / 2; ++j) {
-            if (i % j == 0) {
+        for (int j = 2; j <= i / 2; ++j)
+        {
+            if (i % j == 0)
+            {
                 flag = 0;
                 break;
             }
         }
- 
+
         if (flag == 1)
             cout << i << " ";
     }
@@ -27,9 +30,11 @@ void primeInRange(int R)
 
 int main()
 {
-    
+    int a;
+    cout << "Enter a range ";
+    cin >> a;
 
-    primeInRange(10);
-    
+    primeInRange(a);
+
     return 0;
 }
